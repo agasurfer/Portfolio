014bcdcc3c17f4ff3ac7dae37d25d1ae6b4ff7e0
agasurfer <agasurfer@hotmail.com>
2024-11-15 13:53:39 +0100

    event listeners on navbar

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+//FUNCTION FOR SMOOTH SCROLL WHEN NAVBAR IS CLICKED
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+}); 
